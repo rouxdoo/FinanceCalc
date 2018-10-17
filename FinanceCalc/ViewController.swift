aebc78239c78f67b52ac36c29eddea173617e04b
Ian Jones <ianjones@ians-mac-mini.local>
2018-10-17 17:22:32 -0500

    Initial Commit

---
+++ FinanceCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinanceCalc
+//
+//  Created by Ian Jones on 10/17/18.
+//  Copyright © 2018 Ian Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
