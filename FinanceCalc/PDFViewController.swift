f79d93fffe0fa4ac6408bee5ae1d3df0af88e6e7
Ian Jones <ianjones@ians-mac-mini.local>
2018-10-25 21:49:14 -0500

    recreated preview after failing to commit file ... argh!

--- FinanceCalc/PDFViewController.swift
+++ FinanceCalc/PDFViewController.swift
@@ -10,16 +10,99 @@ import UIKit
 import PDFKit
 
 class PDFViewController: UIViewController {
-    
+
     let documentInteractionController = UIDocumentInteractionController()
     var fin = FinanceStruct(job: 0, renewalCount: 0, renewalAmount: 0, taxRate: 0, apr: 0, term: 0, percentDown: 0)
-    
-    
+    var pdfData: PDFDocument?
 
+    @IBOutlet weak var pdfView: PDFView!
+    
+    @IBAction func sharePdf(_ sender: UIBarButtonItem) {
+        let tmpURL = FileManager.default.temporaryDirectory
+            .appendingPathComponent("SMAC.pdf")
+        pdfView.document?.write(to: tmpURL)
+        documentInteractionController.url = tmpURL
+        documentInteractionController.uti = "com.adobe.pdf"
+        //documentInteractionController.presentPreview(animated: true)
+        documentInteractionController.presentOptionsMenu(from: sender, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let path = Bundle.main.path(forResource: "SMAC Scanned Fillable", ofType: "pdf")
+        let url = URL(fileURLWithPath: path!)
+        if  let pdfDocument = PDFDocument(url: url) {
+            pdfView.displayMode = .singlePageContinuous
+            pdfView.autoScales = true
+            pdfView.document = pdfDocument
+            pdfData = pdfDocument
+            let annotations = pdfDocument.page(at: 0)?.annotations
+            for annotation in annotations! {
+                if (annotation.widgetFieldType == .button) {
+                    continue
+                }
+                switch annotation.fieldName {
+                // this data fills mobility smac fillable -pdfescape.com
+                case "Last Name":
+                    annotation.widgetStringValue = ""
+                case "Service Address":
+                    annotation.widgetStringValue = "Same as above"
+                case "TIL APR":
+                    annotation.widgetStringValue = String(fin.apr)
+                case "TIL Finance Charge":
+                    annotation.widgetStringValue = fin.financeCharge().asCurrency
+                case "TIL Amount Financed":
+                    annotation.widgetStringValue = fin.amountFinanced().asCurrency
+                case "TIL Total of Payments":
+                    annotation.widgetStringValue = fin.totalOfPayments().asCurrency
+                case "TIL Down Payment":
+                    annotation.widgetStringValue = fin.downPayment().asCurrency
+                case "TIL Total Sale Price":
+                    annotation.widgetStringValue = fin.totalFinancedJob().asCurrency
+                case "Number of Payments":
+                    annotation.widgetStringValue = String(fin.term)
+                case "Monthly Payment":
+                    annotation.widgetStringValue = fin.monthlyPayment().asCurrency
+                case "First Payment Date":
+                    let pmtdate = Calendar.current.date(byAdding: .day, value: 45, to: Date())
+                    let formatter = DateFormatter()
+                    formatter.dateFormat = "MM/dd/yyyy"
+                    annotation.widgetStringValue = formatter.string(from: pmtdate!)
+                case "Date":
+                    let today = Calendar.current.date(byAdding: .second, value: 30, to: Date())
+                    let formatter = DateFormatter()
+                    formatter.dateFormat = "MM/dd/yyyy"
+                    annotation.widgetStringValue = formatter.string(from: today!)
+                case "Cash Price":
+                    annotation.widgetStringValue = fin.job.asCurrency
+                case "Renewal Years":
+                    annotation.widgetStringValue = String(fin.renewalCount)
+                case "Renewals per Year":
+                    annotation.widgetStringValue = fin.renewalAmount.asCurrency
+                case "Renewals Total":
+                    annotation.widgetStringValue = fin.renewalsTotal().asCurrency
+                case "Sales Tax":
+                    annotation.widgetStringValue = fin.taxes().asCurrency
+                case "Total Cash Price":
+                    annotation.widgetStringValue = fin.totalCashPrice().asCurrency
+                case "Down Payment":
+                    annotation.widgetStringValue = fin.downPayment().asCurrency
+                case "Amount Financed":
+                    annotation.widgetStringValue = fin.amountFinanced().asCurrency
+                case "Finance Charge":
+                    annotation.widgetStringValue = fin.financeCharge().asCurrency
+                case "Total of Payments":
+                    annotation.widgetStringValue = fin.totalOfPayments().asCurrency
+                case "Total Sale Price":
+                    annotation.widgetStringValue = fin.totalFinancedJob().asCurrency
+                case .none:
+                    annotation.widgetStringValue =  ""
+                case .some(_):
+                    annotation.widgetStringValue = ""
+                }
+            }
+        }
     }
     
 
